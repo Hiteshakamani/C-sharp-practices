2c2ee03516bc152035e9fbe638668532c1800370
Hitesha <kamanihitesha@gmail.com>
2023-04-19 18:36:32 +0530

    18/4/2023

---
+++ C# practices/Interface implementation/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_implementation
+{
+    public interface IShape
+    {
+        void Draw();
+    }
+
+    public interface IColor
+    {
+        void Draw();
+    }
+
+    public class Rectangle : IShape, IColor
+    {
+        void IShape.Draw()
+        {
+            Console.WriteLine("Shape is Rectangle");
+        }
+        void IColor.Draw() 
+        {
+            Console.WriteLine("Color is red");
+        }
+
+    }
+    public class Triangle : IShape,IColor
+    {
+        void IShape.Draw()
+        {
+            Console.WriteLine("Shape is Triangle");
+        }
+        void IColor.Draw()
+        {
+            Console.WriteLine("Color is yellow");
+        }
+
+    }
+    public class Circle : IShape,IColor
+    {
+        void IShape.Draw()
+        {
+            Console.WriteLine("Shape is Circle");
+        }
+        void IColor.Draw()
+        {
+            Console.WriteLine("Color is red");
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape rect = new Rectangle();
+            rect.Draw();
+
+            IColor rect1 = new Rectangle();
+            rect1.Draw();
+
+            IShape tri = new Triangle();
+            tri.Draw();
+
+            IColor tri1 = new Triangle();
+            tri1.Draw();
+
+            IShape cir = new Circle();
+            cir.Draw();
+
+            IColor cir1 = new Circle();
+            cir1.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# practices/Multiple inheritance(interface)/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_inheritance_interface_
+{
+    public interface ITeacher
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+
+        void TeacherInfo();
+    }
+    public interface IStudent
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+
+        void StudentInfo();
+    }
+
+    public class person : IStudent, ITeacher
+    {
+        private string name;
+        private int age;
+
+        string ITeacher.Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        int ITeacher.Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        string IStudent.Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        int IStudent.Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        void IStudent.StudentInfo()
+        {
+            Console.WriteLine($"Student : {name} is {age} years old ");
+        }
+        void ITeacher.TeacherInfo()
+        {
+            Console.WriteLine($"Teacher : {name} is {age} years old ");
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person p1 = new person();
+            ((ITeacher)p1).Name = "abc";
+            ((ITeacher)p1).Age = 30;
+            ((IStudent)p1).Name = "def";
+            ((IStudent)p1).Age = 12;
+            ((ITeacher) p1).TeacherInfo();
+            ((IStudent)p1).StudentInfo();
+            Console.ReadKey();
+        }
+    }
+}
