0df079546083ba38ab63dbe34d7f37920a72c180
Hitesha <kamanihitesha@gmail.com>
2023-04-20 14:52:41 +0530

    20/4/2023

--- C# practices/Abstraction/Program.cs
+++ C# practices/Abstraction/Program.cs
@@ -2,14 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace Abstraction
 {
+    public abstract class Vehical
+    {
+        public string Name { get; set; }
+        public double Speed { get; set; }
+
+        public Vehical(string name , double speed) 
+        { 
+            Name = name;
+            Speed = speed;
+        }
+
+        public abstract void Move();
+    }
+
+    public class car : Vehical
+    {
+        public car (string name, double speed): base (name, speed)
+        { 
+            
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} has speed {Speed}");
+        }
+
+    }
+
+    public class bike : Vehical
+    {
+        public bike(string name, double speed) : base(name, speed)
+        {
+
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} has speed {Speed}");
+        }
+
+    }
+    public class cycle : Vehical
+    {
+        public cycle(string name, double speed) : base(name, speed)
+        {
+
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} has speed {Speed}");
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Vehical car = new car("BMW", 110.45);
+            Vehical bike = new bike("Splender", 60.324);
+            Vehical cycle = new cycle("abc", 50.783);
+
+            car.Move();
+            bike.Move();
+            cycle.Move();
+
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ C# practices/DElegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DElegates
+{
+    public delegate void Message(string message);
+
+    
+    internal class Program
+    {
+
+        public static void showmessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void hidemessage(string message)
+        {
+            Console.WriteLine("👿");
+        }
+        static void Main(string[] args)
+        {
+            Message messagedel;
+            Console.WriteLine("Want meswsage ? ");
+
+            var answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                messagedel = showmessage;
+            }
+            else
+            {
+                messagedel = hidemessage;
+            }
+            messagedel("Hiii....");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
