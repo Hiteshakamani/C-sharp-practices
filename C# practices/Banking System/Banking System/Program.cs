f4e98a430a811c228b0dfda47b3e0d84375b91fb
Hitesha <kamanihitesha@gmail.com>
2023-04-28 11:24:28 +0530

    28/4/2023

---
+++ C# practices/Banking System/Banking System/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_System
+{    
+    
+    //Account class
+    public class Account
+    {
+        public string Name { get; set; }
+        public int Account_number { get; set; }
+        public decimal Balance { get; set; }
+
+        public Account(string name ,int account_number, decimal balance)
+        {
+            Name = name;
+            Account_number = account_number;
+            Balance = balance;
+        }
+    }
+
+    // Bank Class
+    public class Bank
+    {
+        private List<Account> accounts;
+        public string Name { get; set; }
+
+        public Bank(List<Account> accounts, string name)
+        {
+            this.accounts = accounts;
+            Name = name;
+        }
+        // For creating an account
+        public void create_account(string name , decimal balnce)
+        {
+            int account_number = accounts.Count + 1;
+            Account account = new Account(name, account_number, balnce);   
+            accounts.Add(account);
+        }
+         // For deposite
+
+        public void deposite(int account_number,decimal money )
+        {
+            Account account = accounts.Find(a=>a.Account_number == account_number);
+            if(account != null)
+            {
+                account.Balance += money;
+            }
+        }
+
+        //  For Withdraw
+         public bool withdraw(int account_number , decimal money)
+        {
+            Account account = accounts.Find(a=> a.Account_number==account_number);
+            if (account != null)
+            {
+                if(account.Balance > money) 
+                {
+                    account.Balance -= money;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // For check balance
+        public decimal Check_balance(int account_number)
+        {
+            Account account = accounts.Find(a=>a.Account_number==account_number) ;
+            if (account != null)
+            {
+                return account.Balance;
+            }
+            return -1 ;
+        }
+
+    }
+
+    // Main class
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>();
+            Bank bank = new Bank(accounts,"SBI bank");
+            Console.WriteLine("Welcome to " + bank.Name);
+
+            while(true)
+            {
+                Console.WriteLine("Select an Option : ");
+                Console.WriteLine("1. Create account");
+                Console.WriteLine("2. Deposit money ");
+                Console.WriteLine("3. Withdraw money ");
+                Console.WriteLine("4. Check balance ");
+                Console.WriteLine("5. Exit");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter name");
+                            string name=Console.ReadLine();
+                            Console.WriteLine("Enter balance: ");
+                            decimal balance;
+                            if (decimal.TryParse(Console.ReadLine(), out balance))
+                            {
+                                bank.create_account(name, balance);
+                                Console.WriteLine("Account Created Successfully...!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input.");
+                            }
+                            break;
+
+                         case 2:
+                            Console.WriteLine("Enter account number : ");
+                            int account_number;
+                            if (int.TryParse(Console.ReadLine(), out account_number))
+                            {
+                                Console.WriteLine("Enter amount to deposite :");
+                                decimal money;
+                                if (decimal.TryParse(Console.ReadLine(), out money))
+                                {
+                                    bank.deposite(account_number, money);
+                                    Console.WriteLine("DEposite successfull...!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid money....😒");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid money....😒");
+                            }
+                    break;
+                            case 3 :
+                            Console.WriteLine("Enter account number : ");
+                           
+                            if (int.TryParse(Console.ReadLine(), out account_number))
+                            {
+                                Console.WriteLine("Enter amount to withdrw :");
+                                decimal money;
+                                if (decimal.TryParse(Console.ReadLine(), out money))
+                                {
+                                    if(bank.withdraw(account_number, money))
+                                    {
+                                        Console.WriteLine("Withdraw successfull...!");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Insufficient balance....😒");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid money....😒");
+                            }
+
+
+                            break;
+
+
+                        case 4:
+                            Console.WriteLine("Enter account number : ");
+                            if (int.TryParse(Console.ReadLine(), out account_number))
+                            {
+                                decimal account_balance = bank.Check_balance(account_number);
+                                if (account_balance != -1)
+                                {
+                                    Console.WriteLine($"Account balance : {account_balance}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Account not found.");
+                                }
+                            }
+                    
+                            else
+                             {
+                                 Console.WriteLine("Invalid input.");
+                             }
+                    break;
+                        case 5:
+                            Console.WriteLine("Thanks....😊");
+                            
+                            break;
+                        default:
+                            Console.WriteLine("No option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
---
+++ C# practices/MultyThreading/MultyThreading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class ThreadClass
+    {
+        public void ThreadMethod()
+        {
+            for (int i = 0;i< 10; i++)
+            {
+                Console.WriteLine("Thread 1 : " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ThreadClass threadClass = new ThreadClass();
+            Thread thread = new Thread(new ThreadStart(threadClass.ThreadMethod));
+            thread.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread: " + i);
+                Thread.Sleep(500);
+            }
+            Console.ReadKey();
+        }
+    }
+}
